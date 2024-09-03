7e821b8a8affce5491f28378804aca0924fd339a
jonathanhumphrey <jonny105596@outlook.com>
2024-09-03 09:51:12 -0500

    added avatar component/ crunch down list component

---
+++ src/components/UI/Avatar.vue
@@ -0,0 +1,19 @@
+<script setup>
+// Components
+
+
+
+
+</script>
+
+<template>
+    <section class="m-2">
+        <img 
+            src="https://fastly.picsum.photos/id/832/200/200.jpg?hmac=V4CRQyK7KVP2wBYsEhpcpP8wSdwyU5c-yTeMm37uOOo" 
+            alt="profile picture"
+            width="50"
+            height="50"
+            class="rounded-full mr-2"
+        >
+    </section>
+</template>
