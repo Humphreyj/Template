b64e6e2246e7f37e27a64593d2d0d337b9851847
Joshua Humphrey <joshhumphrey1@gmail.com>
2024-09-04 19:02:51 -0500

    wide card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,26 +16,34 @@ const { sampleUser } = storeToRefs(usePrimaryStore())
 </script>
 
 <template>
-    <section class="w-full px-4 primary-text font-display">
-        <section class="w-full grid gap-2 grid-cols-3 mt-8">
+    <section
+        class="w-full max-w-[75rem] mx-auto px-4 primary-text font-display flex-col-ic-js"
+    >
+        <section class="w-full gap-2 flex-ic-je mt-8">
             <Card>
                 <section class="flex-col-ic-js">
-                    <h6 class="">Balance</h6>
-                    <p class="text-2xl">400.50</p>
+                    <h6 class="text-lg">Ammunition</h6>
+                    <p class="text-3xl">54%</p>
                 </section>
             </Card>
             <Card>
                 <section class="flex-col-ic-js">
-                    <h6>Shields</h6>
-                    <p class="text-2xl">80%</p>
+                    <h6 class="text-lg">Shields</h6>
+                    <p class="text-3xl">80%</p>
                 </section>
             </Card>
             <Card>
                 <section class="flex-col-ic-js">
-                    <h6>Current Users</h6>
-                    <p class="text-2xl">12</p>
+                    <h6 class="text-lg">Fuel</h6>
+                    <p class="text-3xl">44%</p>
                 </section>
             </Card>
         </section>
+        <Card container-class="w-full mt-2">
+            <section class="flex-col-ic-js">
+                <h6 class="text-lg">Snacks</h6>
+                <p class="text-3xl">4</p>
+            </section>
+        </Card>
     </section>
 </template>
