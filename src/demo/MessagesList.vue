5fe13abd6363ea2bb178bb90f18d843c74f47011
Josh Humphrey <joshhumphrey@Joshs-Laptop.local>
2024-09-11 16:29:19 -0500

    Started demo messages list

---
+++ src/demo/MessagesList.vue
@@ -0,0 +1,46 @@
+<script setup>
+// Components
+import Card from '@/components/UI/Card.vue'
+// const props = defineProps({})
+// const emit = defineEmits()
+</script>
+
+<template>
+    <Card container-class="mt-2 shadow box-shadow">
+        <section>
+            <header class="px-3 border-b flex-ic-jb h-14">
+                <h4 class="text-xl font-semibold font-display">
+                    Recent Messages
+                </h4>
+                <p>View All</p>
+            </header>
+            <div class="w-full gap-2 pb-2 flex-col-ic-js">
+                <div
+                    class="relative grid items-center w-full h-16 grid-cols-5 px-3 mx-auto font-bold border-b border-slate-500 font-display"
+                >
+                    <p>From</p>
+                    <p>Project</p>
+                    <p>Subject</p>
+                    <p>Date</p>
+                    <p>Read</p>
+                </div>
+                <div
+                    v-for="i in 3"
+                    class="border shadow box-shadow text-lg border-slate-500 py-2 relative h-16 w-[99%] font-semibold font-content rounded-lg mx-auto grid grid-cols-5 items-center justify-between px-2 dark:bg-slate-700"
+                >
+                    <p>User Name</p>
+                    <p>Project</p>
+                    <p>Subject</p>
+                    <p>Date</p>
+                    <p>Read</p>
+                    <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        class="absolute right-10"
+                    />
+                </div>
+            </div>
+        </section>
+    </Card>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import { useToggle } from '@vueuse/core'
 import { storeToRefs } from 'pinia'
 // Components
 import MetricCard from '@/components/cards/MetricCard.vue'
-import Card from '@/components/UI/Card.vue'
+import MessagesList from '@/demo/MessagesList.vue'
 // Pinia
 import { usePrimaryStore } from '../stores/primaryStore'
 // Utils
@@ -40,42 +40,6 @@ const { sampleUser } = storeToRefs(usePrimaryStore())
                 metric-type="number"
             />
         </section>
-        <Card container-class="mt-2 shadow box-shadow">
-            <section>
-                <header class="px-3 border-b flex-ic-jb h-14">
-                    <h4 class="text-xl font-semibold font-display">
-                        Recent Messages
-                    </h4>
-                    <p>View All</p>
-                </header>
-                <div class="w-full gap-2 pb-2 flex-col-ic-js">
-                    <div
-                        class="relative grid items-center w-full h-16 grid-cols-5 px-3 mx-auto font-bold border-b border-slate-500 font-display"
-                    >
-                        <p>User Name</p>
-                        <p>Project</p>
-                        <p>Subject</p>
-                        <p>Date</p>
-                        <p>Read</p>
-                    </div>
-                    <div
-                        v-for="i in 3"
-                        class="border shadow box-shadow text-lg border-slate-500 py-2 relative h-16 w-[99%] font-semibold font-content rounded-lg mx-auto grid grid-cols-5 items-center justify-between px-2 dark:bg-slate-700"
-                    >
-                        <p>User Name</p>
-                        <p>Project</p>
-                        <p>Subject</p>
-                        <p>Date</p>
-                        <p>Read</p>
-                        <input
-                            type="checkbox"
-                            name=""
-                            id=""
-                            class="absolute right-10"
-                        />
-                    </div>
-                </div>
-            </section>
-        </Card>
+        <MessagesList />
     </section>
 </template>
